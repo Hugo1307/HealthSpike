fc1f9daa0e46b0cf01b4cbe459ce8f665432a49a
Alexandre <alexandrepp07@ua.pt>
2022-04-20 17:02:47 +0100

    Distance formula update

--- lib/pt/ua/deti/icm/health_spike/sensors/location.dart
+++ lib/pt/ua/deti/icm/health_spike/sensors/location.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 import 'package:health_spike/pt/ua/deti/icm/health_spike/events/location_events.dart';
 import 'package:health_spike/pt/ua/deti/icm/health_spike/main.dart';
@@ -6,12 +8,30 @@ import 'package:location/location.dart';
 class AppLocationSensor {
   Location location = Location();
 
+  double? _distanceDouble;
+
   LocationData? _currentLocation;
   String _distance = '?', _status = '?';
 
   void onDistanceChanged(Location loc) {
     LocationData locationaData = loc.getLocation() as LocationData;
-    double? _distanceDouble = locationaData.latitude?.toDouble();
+
+    if (_currentLocation != null) {
+      double? latitude1 =
+          (_currentLocation.latitude?.toDouble())! / 57.29577951;
+      double? longitude1 =
+          (_currentLocation.longitude?.toDouble())! / 57.29577951;
+
+      double? latitude2 = (locationaData.latitude?.toDouble())! / 57.29577951;
+      double? longitude2 = (locationaData.longitude?.toDouble())! / 57.29577951;
+
+      _distanceDouble = 3963.0 *
+          acos((sin(latitude1) * sin(latitude2)) +
+              cos(latitude1) * cos(latitude2) * cos(longitude2 - longitude1));
+    } else {
+      _distanceDouble = _distanceDouble! + 0;
+    }
+
     if (_distanceDouble != null) {
       eventBus.fire(DistanceUpdatedEvent(_distanceDouble, DateTime.now()));
     } else {
